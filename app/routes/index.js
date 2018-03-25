81556fc33f7cd985d22671cf4417ee807ea38b31
Rodrigo Garcia Topan Moreira <rodrigo@gruporesolv.com.br>
2018-03-25 19:34:21 -0300

    Configurando Porta Heroku - Controllers não funcionam em produção

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,8 @@
 module.exports = function(application){
 	application.get('/',function(req, response){
 		//response.render("index");
-		application.app.controllers.index.home(application, req, response);//navegando pelos diretórios carregados pelo objeto application
+		response.render('index', {validacao : {}});
+		//application.app.controllers.index.home(application, req, response);//navegando pelos diretórios carregados pelo objeto application
 		//e executando função exportada
 	});
 }
