fcdf56b110fee96eccaf47213a2d6708dbaf3636
Rodrigo Garcia Topan Moreira <rodrigo@gruporesolv.com.br>
2018-03-25 19:50:21 -0300

    tentando acessar controllers com require

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,10 +1,16 @@
 module.exports = function(application){
 	application.post('/chat',function(req, response){
 		//response.render("chat");
-		application.app.controllers.chat.iniciaChat(application, req, response);
+		//O heroku não estava aceitando assim:
+		//application.app.controllers.chat.iniciaChat(application, req, response);
+		//Então eu fiz assim:
+		const chat = require('../controllers/chat');
+		chat.iniciaChat(application, req, response);
 	});
 
 	application.get('/chat',function(req, response){
-		application.app.controllers.chat.iniciaChat(application, req, response);
+		const conversa = require('../controllers/chat');
+		conversa.iniciaChat(application, req, response);
+		//application.app.controllers.chat.iniciaChat(application, req, response);
 	});
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,13 @@
 module.exports = function(application){
 	application.get('/',function(req, response){
 		//response.render("index");
-		response.render('index', {validacao : {}});
-		//application.app.controllers.index.home(application, req, response);//navegando pelos diretórios carregados pelo objeto application
+		//response.render('index', {validacao : {}});
+		//O heroku não estava aceitando assim
+		//application.controllers.index.home(application, req, response);//navegando pelos diretórios carregados pelo objeto application
+		//Então eu fiz assim
+		const home = require('../controllers/index');
+		home.home(application, req, response);//navegando pelos diretórios carregados pelo objeto application
+
 		//e executando função exportada
 	});
 }
